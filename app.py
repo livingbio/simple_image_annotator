341af82a6828f53789eecedc22bcc44d8ca025ad
hauwenw <b97501012@gmail.com>
2018-01-16 15:06:02 +0800

    listen to all public ip

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ if __name__ == "__main__":
     print(files)
     with open("out.csv",'w') as f:
         f.write("image,id,name,xMin,xMax,yMin,yMax\n")
-    app.run(debug="True", port=8000)
+    app.run(debug="True", host='0.0.0.0', port=8000)
